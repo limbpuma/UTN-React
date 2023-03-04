38fc60c676ae2c32537a103f7ef81e0647d6d2a5
Limber Martinez <limbpuma_de@hotmail.com>
2023-03-04 22:44:53 +0100

    change navbar to responsive hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,36 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../styles/General.css';
 
 const Header = () => {
   return (
-    <div>
-      <Navbar bg="light" variant="light" expand="lg" className="navbar-custom">
-        <Container className="me-auto gap-3 text-center">
-          <Navbar.Brand as={Link} to="/" className="nav-custom1 align-items-center justify-content-center">Bienvenido</Navbar.Brand>
-          <Nav>
+    <Navbar bg="light" expand="lg" className="navbar-custom">
+      <Container className="me-auto gap-4 text-center">
+        <Navbar.Brand as={Link} to="/" className="nav-brand-custom">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             <Nav.Link as={Link} to="/ProductList">Productos</Nav.Link>
             <Nav.Link as={Link} to="/Login">Login</Nav.Link>
             <Nav.Link as={Link} to="/Register">Registro</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/">Action</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-        </Container>
-      </Navbar>
-    </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
